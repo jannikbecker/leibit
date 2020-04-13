e4050a248da988cc32ccef4a5fbb49209cb7f7ff
Jannik Becker <ice.3@t-online.de>
2020-04-13 18:47:31 +0200

    Show live trains in system state window

---
+++ Leibit.Client.WPF/Windows/SystemState/ViewModels/LiveTrainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using Leibit.Core.Client.BaseClasses;
+using Leibit.Entities;
+using Leibit.Entities.LiveData;
+
+namespace Leibit.Client.WPF.Windows.SystemState.ViewModels
+{
+    public class LiveTrainViewModel : ViewModelBase
+    {
+
+        #region - Ctor -
+        public LiveTrainViewModel(TrainInformation train)
+        {
+            CurrentTrain = train;
+        }
+        #endregion
+
+        #region - Properties -
+
+        #region [CurrentTrain]
+        public TrainInformation CurrentTrain { get; private set; }
+        #endregion
+
+        //#region [TrainNumber]
+        //public int TrainNumber => CurrentTrain.Train.Number;
+        //#endregion
+
+        //#region [Block]
+        //public string Block => CurrentTrain.Block?.Name ?? string.Empty;
+        //#endregion
+
+        //#region [Station]
+        //public string Station => CurrentTrain.Block?.Track?.Station?.ShortSymbol ?? string.Empty;
+        //#endregion
+
+        //#region [Track]
+        //public string Track => CurrentTrain.Block?.Track?.Name ?? string.Empty;
+        //#endregion
+
+        #region [Direction]
+        public string Direction
+        {
+            get
+            {
+                if (CurrentTrain.Block == null)
+                    return string.Empty;
+
+                switch (CurrentTrain.Direction)
+                {
+                    case eBlockDirection.Left:
+                        return "links";
+                    case eBlockDirection.Right:
+                        return "rechts";
+                    default:
+                        return string.Empty;
+                }
+            }
+        }
+        #endregion
+
+        //#region [LastModified]
+        //public LeibitTime LastModified => CurrentTrain.LastModified;
+        //#endregion
+
+        #endregion
+
+        #region - Public methods -
+
+        #region [Refresh]
+        public void Refresh()
+        {
+            OnPropertyChanged(null);
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
--- Leibit.Client.WPF/Windows/SystemState/ViewModels/SystemStateViewModel.cs
+++ Leibit.Client.WPF/Windows/SystemState/ViewModels/SystemStateViewModel.cs
@@ -1,20 +1,31 @@
 ﻿using Leibit.Client.WPF.Interfaces;
 using Leibit.Client.WPF.ViewModels;
+using Leibit.Client.WPF.Windows.TrainSchedule.ViewModels;
+using Leibit.Client.WPF.Windows.TrainSchedule.Views;
+using Leibit.Core.Client.Commands;
 using Leibit.Entities.Common;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace Leibit.Client.WPF.Windows.SystemState.ViewModels
 {
-    public class SystemStateViewModel : ChildWindowViewModelBase, IRefreshable
+    public class SystemStateViewModel : ChildWindowViewModelBase, IRefreshable, ILayoutSavable
     {
+
+        #region - Needs -
+        private CommandHandler m_DoubleClickCommand;
+        #endregion
 
         #region - Ctor -
         public SystemStateViewModel(Dispatcher dispatcher)
         {
             Dispatcher = dispatcher;
+            LiveTrains = new ObservableCollection<LiveTrainViewModel>();
+            m_DoubleClickCommand = new CommandHandler(__RowDoubleClick, true);
         }
         #endregion
 
@@ -36,12 +47,77 @@ namespace Leibit.Client.WPF.Windows.SystemState.ViewModels
         }
         #endregion
 
+        #region [LiveTrains]
+        public ObservableCollection<LiveTrainViewModel> LiveTrains
+        {
+            get => Get<ObservableCollection<LiveTrainViewModel>>();
+            private set => Set(value);
+        }
+        #endregion
+
+        #region [SaveGridLayout]
+        public bool SaveGridLayout
+        {
+            get
+            {
+                return Get<bool>();
+            }
+            set
+            {
+                Set(value);
+            }
+        }
+        #endregion
+
+        #region [SelectedItem]
+        public LiveTrainViewModel SelectedItem
+        {
+            get
+            {
+                return Get<LiveTrainViewModel>();
+            }
+            set
+            {
+                Set(value);
+            }
+        }
+        #endregion
+
+        #region [DoubleClickCommand]
+        public ICommand DoubleClickCommand
+        {
+            get
+            {
+                return m_DoubleClickCommand;
+            }
+        }
+        #endregion
+
         #endregion
 
         #region - Public methods -
 
         #region [Refresh]
         public void Refresh(Area Area)
+        {
+            __RefreshLoadedEstws(Area);
+            __RefreshLiveTrains(Area);
+        }
+        #endregion
+
+        #region [SaveLayout]
+        public void SaveLayout()
+        {
+            SaveGridLayout = true;
+        }
+        #endregion
+
+        #endregion
+
+        #region - Private methods -
+
+        #region [__RefreshLoadedEstws]
+        private void __RefreshLoadedEstws(Area Area)
         {
             if (Estws == null)
                 Estws = new ObservableCollection<ESTWViewModel>();
@@ -66,6 +142,43 @@ namespace Leibit.Client.WPF.Windows.SystemState.ViewModels
                 if (isNew)
                     Dispatcher.Invoke(() => Estws.Add(current));
             }
+        }
+        #endregion
+
+        #region [__RefreshLiveTrains]
+        private void __RefreshLiveTrains(Area Area)
+        {
+            var currentTrains = new List<LiveTrainViewModel>();
+
+            foreach (var train in Area.LiveTrains)
+            {
+                var current = LiveTrains.FirstOrDefault(t => t.CurrentTrain == train.Value);
+
+                if (current == null)
+                {
+                    current = new LiveTrainViewModel(train.Value);
+                    Dispatcher.Invoke(() => LiveTrains.Add(current));
+                }
+                else
+                    current.Refresh();
+
+                currentTrains.Add(current);
+            }
+
+            var removedTrains = LiveTrains.Except(currentTrains).ToList();
+            Dispatcher.Invoke(() => removedTrains.ForEach(t => LiveTrains.Remove(t)));
+        }
+        #endregion
+
+        #region [__RowDoubleClick]
+        private void __RowDoubleClick()
+        {
+            if (SelectedItem == null)
+                return;
+
+            var Window = new TrainScheduleView(SelectedItem.CurrentTrain.Train.Number);
+            var VM = new TrainScheduleViewModel(Window.Dispatcher, SelectedItem.CurrentTrain.Train);
+            OnOpenWindow(VM, Window);
         }
         #endregion
 
