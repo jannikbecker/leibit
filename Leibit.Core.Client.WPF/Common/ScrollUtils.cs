ab7a0255489a6cd414970d1f54209fbd1f0e401a
Jannik Becker <ice.3@t-online.de>
2020-07-28 00:16:25 +0200

    Fixed scroll behaviour of combobox popup

---
+++ Leibit.Core.Client.WPF/Common/ScrollUtils.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Leibit.Core.Client.Common
+{
+    public class ScrollUtils
+    {
+
+        public static bool GetSuppressScroll(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(SuppressScrollProperty);
+        }
+
+        public static void SetSuppressScroll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(SuppressScrollProperty, value);
+        }
+
+        public static readonly DependencyProperty SuppressScrollProperty = DependencyProperty.RegisterAttached("SuppressScroll", typeof(bool), typeof(ScrollUtils), new PropertyMetadata(false, __OnSuppressScrollChanged));
+
+        private static void __OnSuppressScrollChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue && d is FrameworkElement frameworkElement)
+                frameworkElement.RequestBringIntoView += __StackPanel_RequestBringIntoView;
+        }
+
+        private static void __StackPanel_RequestBringIntoView(object sender, RequestBringIntoViewEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Down) || Keyboard.IsKeyDown(Key.Up))
+                return;
+
+            e.Handled = true;
+        }
+    }
+}
