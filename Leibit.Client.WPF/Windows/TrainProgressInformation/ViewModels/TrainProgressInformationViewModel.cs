8bae4e74f855c974f678737ef851bc6629c77652
Jannik Becker <ice.3@t-online.de>
2021-03-07 16:43:41 +0100

    Fix FMAS

--- Leibit.Client.WPF/Windows/TrainProgressInformation/ViewModels/TrainProgressInformationViewModel.cs
+++ Leibit.Client.WPF/Windows/TrainProgressInformation/ViewModels/TrainProgressInformationViewModel.cs
@@ -595,10 +595,12 @@ namespace Leibit.Client.WPF.Windows.TrainProgressInformation.ViewModels
                     var currentSchedule = liveSchedule.Train.Schedules.LastOrDefault(s => s.IsArrived && s.Schedule.Station.ShortSymbol == liveSchedule.Train.Block.Track.Station.ShortSymbol);
 
                     var isFirstStation = liveSchedule.Train.BlockHistory.Select(b => b.Track.Station).Distinct().Count() == 1
-                        && liveSchedule.Train.Block.Track.CalculateDelay == true
-                        && liveSchedule.Train.Block.Track.IsPlatform == true;
+                        && liveSchedule.Train.Block.Track.CalculateDelay
+                        && liveSchedule.Train.Block.Track.IsPlatform;
 
-                    if (currentSchedule?.Schedule.Handling == eHandling.Start)
+                    if (currentSchedule?.Schedule.Departure == null)
+                        isFirstStation = false;
+                    else if (currentSchedule?.Schedule.Handling == eHandling.Start)
                         isFirstStation = true;
                     else if (liveSchedule.Train.CreatedOn == liveSchedule.Train.Block.Track.Station.ESTW.StartTime)
                         isFirstStation = false;
