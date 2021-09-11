34440d1d2ebb648f745604ec6f036839a3437ed2
Jannik Becker <ice.3@t-online.de>
2021-09-11 23:55:42 +0200

    Skinning for train progress information

--- Leibit.Controls.WPF/LeibitDataGrid/ColumnBackgroundCondition.cs
+++ Leibit.Controls.WPF/LeibitDataGrid/ColumnBackgroundCondition.cs
@@ -3,7 +3,7 @@ using System.Windows.Media;
 
 namespace Leibit.Controls
 {
-    public class ColumnBackgroundCondition
+    public class ColumnBackgroundCondition : DependencyObject
     {
 
         public ColumnBackgroundCondition()
@@ -13,7 +13,17 @@ namespace Leibit.Controls
 
         public ConditionCollection Conditions { get; set; }
 
-        public Brush BackgroundColor { get; set; }
+        //public Brush BackgroundColor { get; set; }
+
+        public Brush BackgroundColor
+        {
+            get { return (Brush)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackgroundColorProperty = DependencyProperty.Register("BackgroundColor", typeof(Brush), typeof(ColumnBackgroundCondition), new PropertyMetadata(Brushes.Transparent));
+
+
 
     }
 }
