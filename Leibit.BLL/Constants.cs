a0ef28796ffb2b5b1a1ee20f429a8c282f5903a2
Jannik Becker <ice.3@t-online.de>
2021-10-06 19:49:22 +0200

    Read train relations from routing information

--- Leibit.BLL/Constants.cs
+++ Leibit.BLL/Constants.cs
@@ -9,6 +9,7 @@ namespace Leibit.BLL
         internal const string SCHEDULE_FOLDER = "Bahnhof Fahrplan";
         internal const string LOCAL_ORDERS_FOLDER = "Bahnhof Anweisungen";
         internal const string TRAIN_COMPOSITION_FOLDER = "Zugbildung";
+        internal const string TRAIN_ROUTING_FOLDER = "Zuglenkung";
         internal const string ESTWONLINE_SETTINGS_FILE = "ESTWonline.ini";
         internal const string ESTWONLINE_DIRECTORY_KEY = "Verz";
         internal const string SERIALIZED_FILE_ENDING = "leibit";
---
+++ Leibit.Entities/Scheduling/TrainRelation.cs
@@ -0,0 +1,17 @@
+﻿using Leibit.Core.Scheduling;
+using System.Collections.Generic;
+
+namespace Leibit.Entities.Scheduling
+{
+    public class TrainRelation
+    {
+        public TrainRelation(int trainNumber)
+        {
+            TrainNumber = trainNumber;
+            Days = new List<eDaysOfService>();
+        }
+
+        public int TrainNumber { get; }
+        public List<eDaysOfService> Days { get; }
+    }
+}
