d71cb11feb74845b148648db8c28c9aef6e71ab7
Jannik Becker <ice.3@t-online.de>
2022-04-30 16:56:59 +0200

    #187 #188 First implementation of SoftwareInfoBLL

---
+++ Leibit.BLL/SoftwareInfoBLL.cs
@@ -0,0 +1,67 @@
+﻿using Leibit.Core.Common;
+using Leibit.Entities;
+using Microsoft.Win32;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Leibit.BLL
+{
+    public class SoftwareInfoBLL : BLLBase
+    {
+
+        #region - Public methods -
+
+        #region [GetSoftwareInfo]
+        public OperationResult<SoftwareInfo> GetSoftwareInfo(string softwareName)
+        {
+            try
+            {
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    return OperationResult<SoftwareInfo>.Ok(new SoftwareInfo { IsInstalled = false });
+
+                var key = __Search(Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"), softwareName);
+
+                if (key == null)
+                    key = __Search(Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"), softwareName);
+
+                if (key == null)
+                    return OperationResult<SoftwareInfo>.Ok(new SoftwareInfo { IsInstalled = false });
+
+                var result = new SoftwareInfo();
+                result.IsInstalled = true;
+                result.DisplayName = key.GetValue("DisplayName")?.ToString();
+                result.DisplayVersion = key.GetValue("DisplayVersion")?.ToString();
+                result.InstallLocation = key.GetValue("InstallLocation")?.ToString();
+                return OperationResult<SoftwareInfo>.Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<SoftwareInfo>.Fail(ex.ToString());
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region - Private helpers -
+
+        #region [__Search]
+        private RegistryKey __Search(RegistryKey key, string softwareName)
+        {
+            foreach (var subKeyName in key.GetSubKeyNames())
+            {
+                var candidate = key.OpenSubKey(subKeyName);
+                var displayName = candidate.GetValue("DisplayName");
+
+                if (displayName != null && displayName.ToString().Contains(softwareName))
+                    return candidate;
+            }
+
+            return null;
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
